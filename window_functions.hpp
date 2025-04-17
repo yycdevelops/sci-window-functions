62e694cb6aae344c25fe87ce7c5732da67798c0b
phi <>
2025-04-16 20:36:57 -0600

    Adding Hanning and Hamming window in cpp

--- window_functions.hpp
+++ window_functions.hpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <cmath>
 
-using namespace std; 
+template<typename T> 
+std::vector<T> hanning_window(size_t num) {
+    std::vector<T> values;
+    
+    for (int i = 0; (i < num); i++) {
+        T value = 0.5 - 0.5 * cos((M_PI * 2 * i / (num - 1)));
+        values.push_back(value); 
+    }
+    return values;
+}
 
-std::vector<float> hanning_window(size_t num) {
-    std::vector<float> values(num);
-
-    for(auto t: values) {
-        float value = 0.5 - 0.5 * (M_PI * 2 * t / (num - 1));
+template<typename T> 
+std::vector<T> hamming_window(size_t num) {
+    std::vector<T> values; 
+    for (int i = 0; (i < num); i++) {
+        T value = 0.54 - 0.46 * cos(M_PI * 2 * i / (num - 1));
         values.push_back(value);
     }
-
     return values;
 }
