fbd4e9ee6883c0b0a2ace04f1e99f1f2506fbe88
phi <>
2025-11-15 08:02:53 -0700

    Some small changes

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,6 @@ use num_traits::{Float, FloatConst};
 
 pub mod window_functions;
 
-pub trait Consts: Float + FloatConst {
+pub trait Consts: Float + FloatConst {}
 
-}
-
-impl Consts for f32 {
-    
-}
+impl Consts for f32 {}
